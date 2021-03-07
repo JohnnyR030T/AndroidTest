9599ce6debec90be6252815a217addfb3b7b60df
Johnathan Myers <johnnyvcrow@gmail.com>
2021-03-06 21:56:55 -0500

    corrected occupation variable

--- app/src/main/java/dev/johnnyrobot/androidtest/MainActivity.kt
+++ app/src/main/java/dev/johnnyrobot/androidtest/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var occupation = developer
+        var occupation = "developer"
         var name = "JohnnyV"
     }
 }
